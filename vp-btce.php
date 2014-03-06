a0bd5eedd0adedcd43fa06e518c87183ff92cec1
Oleg <oleg.rostov@mail.ru>
2014-03-06 22:38:29 +0300

    Create vp-btce.php

---
+++ vp-btce.php
@@ -0,0 +1,74 @@
+<?php
+function vparser($valute)
+{
+$error = "";
+// Получаем котировку USD-Bitcoin
+$url = "https://btc-e.com/api/2/btc_usd/ticker";
+$page = file_get_contents($url);
+if(empty($page))
+	$error = $error."<li>Нет доступа к странице или неверный адрес: ".$url."</li>";
+else
+{
+/* --- Обработка ответа API (доллар) */
+	$buffer = json_decode(trim($page), true);
+/* --- Запись результатов в массив */
+	if($buffer === NULL || $buffer === FALSE)
+		$error = $error."<li>Данные не соответствуют формату JSON ".$url."</li>";
+	else
+	{
+		$result["usd_btc_sale"] = trim(str_replace(",", ".", $buffer['ticker']['sell']));
+		$result["usd_btc_buy"] = trim(str_replace(",", ".", $buffer['ticker']['buy']));
+	};
+};
+$buffer = "";
+
+if($valute != "usd") {
+/* --- Котировки валют СБР */
+	$url = "http://www.sberbank.ru/moscow/ru/quotes/currencies/";
+	$page = file_get_contents($url);
+	if(empty($page))
+		$error = $error."<li>Нет доступа к странице или неверный адрес: ".$url."</li>";
+	else
+	{
+/* --- Обработка контента со страницы */
+		//$page = iconv("UTF-8", "WINDOWS-1251//IGNORE", $page);
+		$regexp = "/<table class=\\\"table3_eggs4\\\"(?:.*)>(.*)<\/table>/Us";
+		preg_match_all($regexp, $page, $buffer1);
+
+		$regexp = "/<td style=\\\"vertical-align:middle;font-size:16px(?:.*)>(.*)<\/td>/Us";
+		preg_match_all($regexp, $buffer1[1][0], $buffer);
+
+/* --- Запись результатов парсинга в массив */
+		$result["usd_sale_sb"] = trim(str_replace(",", ".", $buffer[1][0]));
+		$result["usd_buy_sb"] = trim(str_replace(",", ".", $buffer[1][1]));
+		$result["eur_sale_sb"] = trim(str_replace(",", ".", $buffer[1][2]));
+		$result["eur_buy_sb"] = trim(str_replace(",", ".", $buffer[1][3]));
+	};
+$buffer = "";	
+};
+if(empty($error))
+	return $result;
+else
+	return $error;
+};
+?>
+
+<?php
+$result = vparser($_POST["valute"]);
+/*  Конвертер валют  */
+	switch ($_POST["valute"]) {
+	case "rur":
+		$kotirovka = $result["usd_btc_sale"]*$result["usd_buy_sb"];
+		break;
+	case "usd":
+		$kotirovka = $result["usd_btc_sale"];
+		break;
+	case "eur":
+		$kotirovka = $result["usd_btc_sale"]*($result["usd_sale_sb"]/$result["eur_buy_sb"]);
+		break;
+	default:
+		$kotirovka = 1;
+		$error = $error."<li>Конвертер не работает с валютой ".$valute."</li>";	
+	};
+echo $kotirovka;
+?>
