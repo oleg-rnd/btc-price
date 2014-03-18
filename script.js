d1bb432e179b33e1316befe46695675e25563efe
Oleg <oleg.rostov@mail.ru>
2014-03-18 22:01:36 +0300

    Update script.js
    change if NaN

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 <script type="text/javascript">
+// Проверка на число
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
+//var i = 0;
 function vconvertor(kotirovka, name_class) {
 /* Конвертация валюты в биткоин и замена цен */
 var regexp = /(\d+.)/ig;
@@ -14,20 +19,41 @@ $('.'+name_class).each(function () {
 	$(this).removeClass("active").addClass("noactive");
 	$(this).next($('.btc_price')).text(new_val+' BTC').removeClass("noactive").addClass("active");
 });
+//i = i+1;
+//$('.result').text(kotirovka+" "+i);
 };	 
 $(document).ready(function(){
-var page_valute = "eur"; // initial currency on the page - начальная валюта на странице (usd, eur, rur, cny)
-var url_parser = "vp-btce.php"; // link to the parser for quotations (choice of the exchange) - ссылка на парсер для получения котировок (выбор биржи)
-//var url_parser = "vp-bitstamp.php";
-//var url_parser = "vp-huobi.php";
-var name_class = "price"; // container class with a price - класс контейнеров с ценой
-var time_int = 5000; // interval between runs (ms) - значение интервала между запусками (мс)
+var page_valute = "eur"; // начальная валюта на странице (usd, eur, rur, cny )
+//var url_parser = "vp-huobi.php"; 
+//  var url_parser = "vp-huobi-1.php";  альтернативный вариант, var url_parser = "vp-btce.php"; 
+var url_parser = "vp-bitstamp.php"; // парсер для получения котировок (выбор биржи)
+var name_class = "price"; // класс контейнера с ценой
+var time_int = 2000; // значение интервала между запусками (мс)
+var auto_run = false; // режим автозапуска (true или false)
 
 $('.'+name_class).after(function(index){
     return '<span class="btc_price noactive"></span>';
 });
 
 var timer = 0;
+
+if(auto_run) {
+$(function(){
+// Запуск конвертора в авто режиме
+timer = setTimeout(function run() {
+$.ajax({
+  type: "POST",
+  url: url_parser,
+  data: "valute="+page_valute,
+  success: function(kotirovka){
+	if(isNumeric(kotirovka) && kotirovka != 0)
+		vconvertor(kotirovka, name_class);
+  }
+});
+    timer = setTimeout(run, time_int);
+  }, 10);
+});
+};
 
 $('#start_convert').click(function(){
 // Запуск конвертора по таймеру после клика
@@ -37,7 +63,8 @@ $.ajax({
   url: url_parser,
   data: "valute="+page_valute,
   success: function(kotirovka){
-	vconvertor(kotirovka, name_class);
+	if(isNumeric(kotirovka) && kotirovka != 0)
+		vconvertor(kotirovka, name_class);
     /*alert("Котировка получена: " + kotirovka);*/
   }
 });
